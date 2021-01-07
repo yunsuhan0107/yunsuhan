d627cedd62e4c31887d7f7e7451f0ed49c91ae0f
yunsuhan0107 <46840483+yunsuhan0107@users.noreply.github.com>
2021-01-07 18:16:10 +0900

    changed contact to original

--- assets/php/contact-form.php
+++ assets/php/contact-form.php
@@ -1,20 +1,16 @@
 <?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $to = 'me@yunsuhan.com'; //<-- Enter your E-Mail address here.
+    $subject = $_POST['subject'];
 
-if (isset($_POST['submit'])){
-  $name = $_POST['name'];
-  $subject = $_POST['subject'];
-  $mailFrom = $_POST['email'];
-  $message = $_POST['message'];
+    $body = "From: $name <br> E-Mail: $email <br> Message: <br> $message";
 
-  $mailTo = "me@yunsuhan.com";
-  $headers = "From: ".$mailFrom;
-  $text = "You have received an email from ".$name.".\n\n".$message;
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+    $headers .= 'From:' . $email. "\r\n";
+    $headers .= 'Cc:' . $email. "\r\n";
 
-
-  mail($mailTo, $subject, $text, $headers);
-  header("Location: contact.php?mailsend");
-}
-
-
-
- ?>
+    mail($to, "New Message from Website: $subject", $body, $headers);
+?>
